fca99023c87941c1fe5e22a71f438fbaad7a5501
[jiansorge] <[ji@nsorge.com]>
2022-11-19 12:27:44 -0800

    add new About component and routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,15 +5,19 @@ import Header from './header';
 
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
-import Project from '../routes/project';
+import Project from '../routes/projects';
+import About from '../routes/about';
+
 
 const App = () => (
 	<div id="app">
         <Header />
         <Router>
             <Route path="/" component={Home} />
-            <Route path="/project/" component={Project} name="me" />
-            <Route path="/project/:name" component={Project} />
+            <Route path="/projects/" component={Project} name="All Projects" />
+            <Route path="/projects/:name" component={Project} />
+            <Route path="/about/" component={About} />
+
         </Router>
     </div>
 );
---
+++ src/routes/about/index.tsx
@@ -0,0 +1,34 @@
+import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import style from './style.css';
+
+const About = () => {
+    const [time, setTime] = useState<number>(Date.now());
+    const [count, setCount] = useState<number>(0);
+
+    useEffect(() => {
+		const timer = setInterval(() => setTime(Date.now()), 1000);
+		return () => clearInterval(timer);
+	}, []);
+
+    return (
+		<div class={style.about}>
+			<h1>About Jian</h1>
+			<p>I am a front-end web developer.</p>
+
+			<div>Current time: {new Date(time).toLocaleString()}</div>
+
+			<p>
+				<button onClick={() => setCount((count) => count + 1)}>Click Me</button>
+				{' '}
+				Clicked {count} times.
+			</p>
+
+			<section>
+				Other facts about me
+			</section>
+		</div>
+	);
+};
+
+export default About;
