2d1bf0b09b43239738c9cdcdee8904ea755a609f
[jiansorge] <[ji@nsorge.com]>
2022-11-23 20:59:46 -0800

    new project html + classes

--- src/routes/projects/project.tsx
+++ src/routes/projects/project.tsx
@@ -19,18 +19,81 @@ const Project = ({ name }: Props) => {
 
     return (
 		<div class={style.project}>
-			<h1>Project: {name}</h1>
-			<p>This is the  project named { name }.</p>
+            <div class="relative flex flex-col-reverse py-16 lg:py-0 lg:flex-col">
+            <div class="w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:py-20 lg:max-w-screen-xl">
+                <div class="mb-0 lg:max-w-lg lg:pr-8 xl:pr-6">
+                <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:text-center">
+                    The quick, brown fox<br class="hidden md:block" />
+                    jumps over a lazy dog
+                </h2>
+                <p class="mb-5 text-base text-gray-700 md:text-lg md:text-center">
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+                </p>
+                <div class="mb-10 text-center md:mb-16 lg:mb-20">
+                    <a
+                    href="/"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                    Learn more
+                    </a>
+                </div>
 
-			<div>Current time: {new Date(time).toLocaleString()}</div>
+                </div>
+            </div>
+            <div class="inset-y-0 top-0 right-0 w-full max-w-xl px-4 mx-auto mb-6 md:px-0 lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+                <img
+                class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+                src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                alt=""
+                />
+            </div>
+            </div>
 
-			<p>
-				<button onClick={() => setCount((count) => count + 1)}>Click Me</button>
-				{' '}
-				Clicked {count} times.
-			</p>
+            <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 
-			<h2>Key Takeaways</h2>
+                <div class="grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-4 sm:grid-cols-2">
+                    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+                        <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                            <h6 class="mb-2 font-semibold leading-5">The doctor said</h6>
+                            <p class="text-sm text-gray-900">
+                                Sportacus andrew weatherall goose Refined gentlemen super mario des lynam alpha trion zap rowsdower.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+                        <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                            <h6 class="mb-2 font-semibold leading-5">Skate ipsum dolor</h6>
+                            <p class="text-sm text-gray-900">
+                                Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+                        <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                            <h6 class="mb-2 font-semibold leading-5">Baseball ipsum dolor</h6>
+                            <p class="text-sm text-gray-900">
+                                Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+                        <div class="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                            <h6 class="mb-2 font-semibold leading-5">They urge you</h6>
+                            <p class="text-sm text-gray-900">
+                                A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <a
+                    href="/"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                        Learn more
+                    </a>
+                </div>
+            </div>
             <Footer />
 		</div>
 	);
