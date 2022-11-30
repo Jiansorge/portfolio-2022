e8dfe798f86e3f054cd9a76c415cf2c8bb8f083b
[jiansorge] <[ji@nsorge.com]>
2022-11-29 19:29:45 -0800

    adjust home page sections

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => (
 		<div className="hero min-h-screen bg-base-200">
 			<div className="hero-content text-center">
 				<div className="max-w-md">
-				<h1 className="text-5xl font-bold">Hello!</h1>
+				<h1 className="text-5xl font-bold">Hello! </h1>
 				<p className="py-6">My name is Jian.</p>
 				<button className="btn btn-primary">Check out my portfolio!</button>
 				</div>
@@ -15,7 +15,7 @@ const Home = () => (
 		</div>
 
 		<section>
-			<div className="carousel w-full">
+			{/* <div className="carousel w-full">
 				<div id="slide1" className="carousel-item relative w-full">
 					<img src="https://placeimg.com/800/200/arch" className="w-full" />
 					<div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -44,7 +44,7 @@ const Home = () => (
 						<a href="#slide1" className="btn btn-circle">❯</a>
 					</div>
 				</div>
-			</div>
+			</div> */}
 		</section>
 
 	</div>
