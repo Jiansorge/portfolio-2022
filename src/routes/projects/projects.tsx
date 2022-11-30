4a3529eb5734e3494a5764040e290d854e376813
[jiansorge] <[ji@nsorge.com]>
2022-11-29 19:34:28 -0800

    update text

--- src/routes/projects/projects.tsx
+++ src/routes/projects/projects.tsx
@@ -10,18 +10,12 @@ const Projects = () => {
                 <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
                     <h2 className="max-w-lg mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
                         <span className="inline-block mb-1 sm:mb-4">
-                            There is no must in art
-                            <br className="hidden md:block" />
-                            because art is free.
+                            All Projects 
                         </span>
                         <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
                     </h2>
                     <p className="text-gray-700 lg:text-sm lg:max-w-md">
-                        "Class is dead", says Foucault; however, according to Reicher, it is
-                        not so much class that is dead, but rather the absurdity, and some
-                        would say the paradigm, of class. However, the subject is interpolated
-                        into a postpatriarchialist dialectic theory that includes
-                        consciousness as a totality.
+                        Here are some of my portfolio highlights.
                     </p>
                 </div>
                 <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
@@ -138,7 +132,7 @@ const Projects = () => {
                             </div>
                         </a>
                         <h2 className="font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl">
-                            <span className="inline-block mb-2">The brown fox</span>
+                            <span className="inline-block mb-2">More Projects</span>
                             <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
                         </h2>
                     </div>
