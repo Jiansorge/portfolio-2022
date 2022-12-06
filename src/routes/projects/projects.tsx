6129e7981a67a9ce8e7b5da21824201afdc62a02
[jiansorge] <[ji@nsorge.com]>
2022-12-05 20:02:41 -0800

    hide mini tiles

--- src/routes/projects/projects.tsx
+++ src/routes/projects/projects.tsx
@@ -50,7 +50,7 @@ const Projects = () => {
                 </div>
             </div>
 
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            {/* <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="flex flex-col w-full mb-6 lg:justify-between lg:flex-row md:mb-8">
                     <div className="flex items-center mb-5 md:mb-6 group lg:max-w-xl">
                         <a href="/" aria-label="Item" className="mr-3">
@@ -102,7 +102,7 @@ const Projects = () => {
                     </svg>
                     </a>
                 </div>
-            </div>
+            </div> */}
 		</div>
 	);
 };
