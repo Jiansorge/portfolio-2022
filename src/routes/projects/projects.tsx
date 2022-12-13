84f356caf65c402e64a286a51370fbbf07fa8700
[jiansorge] <[ji@nsorge.com]>
2022-12-12 20:40:16 -0800

    update logic for minitile

--- src/routes/projects/projects.tsx
+++ src/routes/projects/projects.tsx
@@ -77,7 +77,7 @@ const Projects = () => {
                 </div>
             </div>
 
-            { Object.keys(projects).length > 1 &&
+            { Object.keys(projects).length > 4 &&
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="flex flex-col w-full mb-6 lg:justify-between lg:flex-row md:mb-8">
                         <div className="flex items-center mb-5 md:mb-6 group lg:max-w-xl">
