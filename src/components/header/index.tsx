23c6d91010313a82341b59efbb8fb8fc31784dbf
[jiansorge] <[ji@nsorge.com]>
2022-12-15 19:46:57 -0800

    add header hyperlink

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,10 +4,10 @@ import style from './style.css';
 
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-indigo">
-		<div class="flex items-center space-x-4 p-4">
+		<a class="flex items-center space-x-4 p-4" href="/">
 			<img src="./assets/icons/android-chrome-512x512.png" alt="" width="50" />
 			<h1 class="">Jian Sorge's Portfolio</h1>
-		</div>
+		</a>
 
 		<nav class="px-4 space-x-4">
 			<Link activeClassName={style.active} class="" href="/">Home</Link>
