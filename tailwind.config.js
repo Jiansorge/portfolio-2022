98be9b913531f5980f31320f60e97c96940c254c
[jiansorge] <[ji@nsorge.com]>
2022-12-15 19:38:47 -0800

    add new tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./src/**/*.{html,js,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      pink: '#ff9683',
+      purple: '#212035',
+      offWhite: '#f7f7f7',
+      indigo: '#98A1E9',
+      mainGrey: '#ececec',
+      darkGrey: '#afafaf',
+    }
   },
   plugins: [require("daisyui")],
 }
