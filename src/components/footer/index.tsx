d58c5560b9b4b798cef57f9a89e263e501195bea
[jiansorge] <[ji@nsorge.com]>
2022-12-21 22:03:08 -0800

    adjust footer padding

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { h } from 'preact';
 
 const Footer = () => {
 	return(
-		<footer className="footer footer-center p-10 bg-purple text-indigo">
+		<footer className="footer footer-center pt-6 pb-10 bg-purple text-indigo">
 			<div>
 				<img src="../../assets/icons/footer-logo.svg" alt="" />
 				<p className="font-bold">
