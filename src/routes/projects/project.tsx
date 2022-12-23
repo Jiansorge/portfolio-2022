9fe02467b156ec8255a7af42a2bfa4b78f3e1b41
[jiansorge] <[ji@nsorge.com]>
2022-12-23 11:56:17 -0800

    project page spacing

--- src/routes/projects/project.tsx
+++ src/routes/projects/project.tsx
@@ -9,7 +9,7 @@ const Project = () => {
     const project:ProjectType = projectList[projectPath];
     return (
 		<div class="text-yellow">
-            <div class="relative flex flex-col-reverse py-16 lg:py-0 lg:flex-col">
+            <div class="relative flex flex-col-reverse pt-16 pb-4 lg:py-0 lg:flex-col">
                 <div class="w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:py-20 lg:max-w-screen-xl">
                     <div class="mb-0 pt-6 lg:max-w-lg lg:pr-8 xl:pr-6">
                         <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-pink sm:text-4xl sm:leading-none md:text-center">
@@ -41,13 +41,23 @@ const Project = () => {
                         }
                         {
                             project.gitUrl
-                            && <div class="mb-10 text-center md:mb-16 lg:mb-20">
+                            && <div class="m-8 text-center">
                                 <a
                                 href={project.gitUrl}
                                 class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                 >
-                                    Git Repo
-                            </a>
+                                    <button class="px-6 py-2 transition-colors duration-200  border border-teal bg-purple hover:bg-teal hover:text-purple rounded-lg">
+                                        Git Repo
+                                        <svg
+                                        className="inline-block w-3 ml-2"
+                                        fill="currentColor"
+                                        viewBox="0 0 12 12"
+                                        >
+                                            <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                                        </svg>
+                                    </button>
+                                </a>
+
                             </div>
                         }
                     </div>
