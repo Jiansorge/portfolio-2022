560cd95488a1ca7fc073d54beef634c3f1bff565
[jiansorge] <[ji@nsorge.com]>
2022-12-23 10:49:09 -0800

    faq styling and content

--- src/routes/about/faq.tsx
+++ src/routes/about/faq.tsx
@@ -45,14 +45,14 @@ const Item = ({ title, children }: Props) => {
   };
   export const Faq = () => {
     return (
-      <div class="px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+      <div class="px-4 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-20 ">
         <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
           <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-indigo sm:text-4xl md:mx-auto">
               FAQ
             </h2>
             <p class="text-base text-pink md:text-lg ">
-              What would you like to know about me?
+              
             </p>
           </div>
           <div class="space-y-4">
