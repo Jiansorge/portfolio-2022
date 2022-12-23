de949f3b6abb76070a0f1cee25b6ba0f1339af76
[jiansorge] <[ji@nsorge.com]>
2022-12-23 12:07:10 -0800

    update icon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,11 +5,11 @@ import style from './style.css';
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
 		<a class="flex items-center space-x-4 p-4" href="/">
-			<img src="./assets/icons/android-chrome-512x512.png" alt="" width="50" />
+			<img src="./assets/icon.png" alt="" width="50" />
 			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
 		</a>
 
-		<nav class="px-4 space-x-4 font-bold h-full">
+		<nav class="px-4 space-x-4 font-bold h-full text-indigo">
 			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/">Home</Link>
 			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
 			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/about">About</Link>
