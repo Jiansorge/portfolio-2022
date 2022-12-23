52eaf16d6ea49c58118276ba026caa317e605ff7
[jiansorge] <[ji@nsorge.com]>
2022-12-23 11:27:31 -0800

    header font style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,10 +6,10 @@ const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
 		<a class="flex items-center space-x-4 p-4" href="/">
 			<img src="./assets/icons/android-chrome-512x512.png" alt="" width="50" />
-			<h1 class="">Jian Sorge's Portfolio</h1>
+			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
 		</a>
 
-		<nav class="px-4 space-x-4">
+		<nav class="px-4 space-x-4 font-bold">
 			<Link activeClassName={style.active} class="" href="/">Home</Link>
 			<Link activeClassName={style.active} href="/projects">Projects</Link>
 			<Link activeClassName={style.active} href="/about">About</Link>
