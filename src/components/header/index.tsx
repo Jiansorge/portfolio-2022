530af3bec9433a22b5fba23e9f61923cfbd75859
[jiansorge] <[ji@nsorge.com]>
2022-12-23 17:34:26 -0800

    add nav active bg color

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,9 +10,9 @@ const Header = () => (
 		</a>
 
 		<nav class="px-4 py-6 font-bold h-full text-indigo">
-			<Link activeClassName="text-pink bg-[rgb(92,113,222)]" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/">Home</Link>
-			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
-			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/about">About</Link>
+			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/">Home</Link>
+			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
+			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/about">About</Link>
 		</nav>
 	</header>
 );
