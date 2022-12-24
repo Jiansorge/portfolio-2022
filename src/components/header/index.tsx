694de35a8d33068655678f65d09b56cc0fa33efa
[jiansorge] <[ji@nsorge.com]>
2022-12-23 21:18:37 -0800

    fix disappearing logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'preact-router/match';
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
 		<a class=" hover:text-pink transition-colors duration-200 flex items-center space-x-2 px-6" href="/">
-			<img src="./assets/icon.png" alt="" width="50" />
+			<img src="/assets/icon.png" alt="" width="50" height="50" />
 			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
 		</a>
 
