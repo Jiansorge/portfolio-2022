9cf77fe7485068fa20daf27f66f48e06b2ed491a
[jiansorge] <[ji@nsorge.com]>
2022-12-23 19:02:00 -0800

    header spacing

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,9 +4,9 @@ import style from './style.css';
 
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
-		<a class="flex items-center space-x-4 px-6" href="/">
+		<a class=" hover:text-pink transition-colors duration-200 flex items-center space-x-2 px-6" href="/">
 			<img src="./assets/icon.png" alt="" width="50" />
-			<h1 class="font-bold hover:text-pink transition-colors duration-200">Jian Sorge's Portfolio</h1>
+			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
 		</a>
 
 		<nav class="px-4 py-6 font-bold h-full text-indigo">
