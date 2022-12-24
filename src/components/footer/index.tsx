6c635f78f0c6e495d2e3603907b056e26c339394
[jiansorge] <[ji@nsorge.com]>
2022-12-24 10:15:58 -0800

    responsive footer and header

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
 				{/* <img src="../../assets/icons/footer-logo.svg" alt="" /> */}
 				<a href="/" className="hover:text-pink transition-colors duration-200">Jian Sorge</a>
 				<span>|</span>
-				<span>Front-end Web Developer</span>
-				<span>|</span>
+				<span class="hidden sm:inline-block">Front-end Web Developer</span>
+				<span class="hidden sm:inline-block">|</span>
 				<div className="grid grid-flow-col gap-4">
 					<a href="https://github.com/Jiansorge" target="_blank" rel="noopener noreferrer">
 						<img src="../../assets/icons/github.svg" alt="Jian's Github" />
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,15 +4,15 @@ import { Link } from 'preact-router/match';
 
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
-		<a class=" hover:text-pink transition-colors duration-200 flex items-center space-x-2 px-6" href="/">
+		<a class="hover:text-pink transition-colors duration-200 flex items-center space-x-2 px-6" href="/">
 			<img src="/assets/icon.png" alt="" width="50" height="50" />
-			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
+			<h1 class="font-bold">Jian<span class="hidden sm:inline-block">&nbsp;Sorge</span>'s Portfolio</h1>
 		</a>
 
 		<nav class="px-4 py-6 font-bold h-full text-indigo">
-			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/">Home</Link>
-			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
-			<Link activeClassName="text-pink bg-blue" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/about">About</Link>
+			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/">Home</Link>
+			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
+			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/about">About</Link>
 		</nav>
 	</header>
 );
