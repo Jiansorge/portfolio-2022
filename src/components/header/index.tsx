45095bc63bc9df8b2147cee197211ee8ed12be75
[jiansorge] <[ji@nsorge.com]>
2022-12-23 17:11:50 -0800

    nav spacing

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,15 +4,15 @@ import style from './style.css';
 
 const Header = () => (
 	<header class="bg-purple flex justify-between items-center text-teal">
-		<a class="flex items-center space-x-4 px-6 py-4" href="/">
+		<a class="flex items-center space-x-4 px-6" href="/">
 			<img src="./assets/icon.png" alt="" width="50" />
 			<h1 class="font-bold">Jian Sorge's Portfolio</h1>
 		</a>
 
-		<nav class="px-4 space-x-4 font-bold h-full text-indigo">
-			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/">Home</Link>
-			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
-			<Link activeClassName="text-pink" class="hover:text-yellow transition-colors duration-200" href="/about">About</Link>
+		<nav class="px-4 py-6 font-bold h-full text-indigo">
+			<Link activeClassName="text-pink bg-[rgb(92,113,222)]" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/">Home</Link>
+			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/projects">Projects</Link>
+			<Link activeClassName="text-pink" class="px-2 py-6 hover:text-yellow transition-colors duration-200" href="/about">About</Link>
 		</nav>
 	</header>
 );
