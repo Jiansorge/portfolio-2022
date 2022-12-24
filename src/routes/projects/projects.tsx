4b685d820ca0b751014bd39b28efc84976d30873
[jiansorge] <[ji@nsorge.com]>
2022-12-23 16:34:11 -0800

    projects header spacing

--- src/routes/projects/projects.tsx
+++ src/routes/projects/projects.tsx
@@ -37,11 +37,11 @@ const Projects = () => {
 		<div class="text-yellow">
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="flex flex-col mb-6 lg:justify-between lg:flex-row md:mb-8">
-                    <h2 className="max-w-lg mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
-                        <span className="inline-block mb-1 sm:mb-4 text-pink">
+                    <h2 className="text-pink max-w-lg mb-8 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none md:mb-6 group">
+                        {/* <span className="inline-block mb-1 sm:mb-4 text-pink"> */}
                             All Projects 
-                        </span>
-                        <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
+                        {/* </span> */}
+                        {/* <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" /> */}
                     </h2>
                     <p className="lg:text-sm lg:max-w-md">
                         Here are some of my portfolio highlights.
