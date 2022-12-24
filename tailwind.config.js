8d27978800b740a539b2b6573c7827ec6467ff34
[jiansorge] <[ji@nsorge.com]>
2022-12-24 10:34:05 -0800

    add rest of screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,11 @@ module.exports = {
     },
     screens: {
       xs: '380px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
 
     },
   },
