bc5ffd296fb8f4b486048c0e2f32f8a41075edbb
[jiansorge] <[ji@nsorge.com]>
2023-05-07 11:48:41 -0700

    update footer spacing

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,13 +3,13 @@ import { h } from 'preact';
 
 const Footer = () => {
 	return(
-		<footer className="footer footer-center py-6 bg-purple flex justify-center text-indigo">
+		<footer className="footer footer-center space-x-3 py-6 bg-purple flex justify-center text-indigo">
 				{/* <img src="../../assets/icons/footer-logo.svg" alt="" /> */}
 				<a href="/" className="hover:text-pink transition-colors duration-200">Jian Sorge</a>
 				<span>|</span>
 				<span class="hidden sm:inline-block">Front-end Web Developer</span>
 				<span class="hidden sm:inline-block">|</span>
-				<div className="grid grid-flow-col gap-4">
+				<div className="grid grid-flow-col space-x-2">
 					<a href="https://github.com/Jiansorge" target="_blank" rel="noopener noreferrer">
 						<img src="../../assets/icons/github.svg" alt="Jian's Github" height="24" width="24" />
 					</a> 
