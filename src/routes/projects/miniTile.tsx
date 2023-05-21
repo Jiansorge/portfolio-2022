9373b2dad510161b684fafe25242dd9f8ff89b4b
[jiansorge] <[ji@nsorge.com]>
2023-05-21 12:35:50 -0700

    update more projects styling

--- src/routes/projects/miniTile.tsx
+++ src/routes/projects/miniTile.tsx
@@ -10,13 +10,13 @@ interface Props {
 const MiniTile = ({ project, href }: Props) => {
     const thumbUrl =`${project.imgUrl.slice(0,-4)}-min.png`;
 	return (
-        <a href={href} aria-label="View Item">
+        <a href={href} aria-label="View Item" className="flex flex-col items-center">
             <img
-                className="object-top w-auto  text-center h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80"
+                className="object-top h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80 "
                 src={thumbUrl}
                 alt={project.imgAlt}
             />
-            <p className="mb-2 text-xl font-bold leading-none sm:text-2xl text-pink">
+            <p className="mb-2 text-xl font-bold leading-none sm:text-2xl text-pink self-start">
                 {project.name}
             </p>
             <p className="text-gray-700">
