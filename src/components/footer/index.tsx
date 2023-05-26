a73ae8d606e44869be8b527a43eba68f19f26d99
[jiansorge] <[ji@nsorge.com]>
2023-05-25 20:16:43 -0700

    add header and footer animations

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
 	return(
 		<footer className="drop-shadow-2xl footer footer-center space-x-3 py-6 bg-purple flex justify-center text-indigo">
 				{/* <img src="../../assets/icons/footer-logo.svg" alt="" /> */}
-				<a href="/" className="hover:text-pink transition-colors duration-200 drop-shadow-md">Jian Sorge</a>
+				<a href="/" className="hover:text-yellow transition-colors duration-200 drop-shadow-md">Jian Sorge</a>
 				<span>|</span>
-				<span class="hidden sm:inline-block drop-shadow-md">Front-end Web Developer</span>
+				<span class="hidden sm:inline-block drop-shadow-md hover:animate-pulse">Front-end Web Developer</span>
 				<span class="hidden sm:inline-block">|</span>
 				<div className="grid grid-flow-col space-x-2">
 					<a href="https://github.com/Jiansorge" target="_blank" rel="noopener noreferrer">
@@ -15,7 +15,7 @@ const Footer = () => {
 					</a> 
 
 					<a href="https://www.linkedin.com/in/jiansorge/" target="_blank" rel="noopener noreferrer">
-						<img src="../../assets/icons/linkedin.svg" alt="Jian's LinkedIn" height="24" width="24" />
+						<img src="../../assets/icons/linkedin.svg" alt="Jian's LinkedIn" height="24" width="24"/>
 					</a>
 
 					<a href="https://twitter.com/code_with_jian" target="_blank" rel="noopener noreferrer">
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'preact-router/match';
 
 const Header = () => (
 	<header class="drop-shadow-md bg-purple flex justify-between items-center text-indigo ">
-		<a class="hover:text-yellow transition-colors duration-200 flex items-center space-x-2 px-2 xs:px-6" href="/">
+		<a class="hover:text-yellow transition-colors hover:animate-pulse duration-200 flex items-center space-x-2 px-2 xs:px-6" href="/">
 			<img class="hidden xs:inline-block" src="/assets/icons/header.png" alt="" width="50" height="50" />
 			<h1 class="font-bold drop-shadow-md">Jian<span class="hidden sm:inline-block">&nbsp;Sorge</span>'s Portfolio</h1>
 		</a>
